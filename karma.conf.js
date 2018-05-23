e475f8133cdb03cf8c6aca8cd3441b245396a56d
Hubot <zach.gish@hotmail.com>
2018-05-22 21:14:42 -0600

    new file

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,21 @@
 "use strict";
 
-module.exports= function(config) ({
-    basePath: "",
-    frameworks: ["mocha","browserify"],
-
-})
+module.exports= function(config) {
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "browserify"],
+        files: ["src/*.js","test/*.js"],
+        exclude:[],
+        preprocessors:{
+            "test/*.js":["browserify"],
+            "src/*.js":["browserify"]
+        },
+        reporters: ["progress", "mocha"],
+        port:3036,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ["Chrome"],
+        singleRun: false,
+        concurrency: Infinity,
+    });
+};
