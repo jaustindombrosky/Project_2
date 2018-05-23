cca4e6f7e5c5967918f2a26351ac851bb10731be
Kailey Tetrick <kaileytetrick88@gmail.com>
2018-05-22 20:32:41 -0600

    login js and associated edits to html

--- mini-upload-form/assets/js/login.js
+++ mini-upload-form/assets/js/login.js
@@ -38,20 +38,16 @@ $(document).ready(function() {
     });
   
     
-    $("#createButton").on("click", function(event) {
+    $("#loginButton").on("click", function(event) {
   
-      var newEmail = $("#inputEmail").val();
-      var newPassword = $("#inputPassword").val();
-      var repeatPassword = $("#repeatPassword").val();
-      var newUsername= $("#inputUsername").val();
-      console.log(newEmail);
-      console.log(newPassword);
-      console.log(repeatPassword);
-      console.log(newUsername);
+      var userEmail = $("#inputEmail").val();
+      var userPassword = $("#inputPassword").val();
+      console.log(userEmail);
+      console.log(userPassword);
   
       firebase
         .auth()
-        .signInWithEmailAndPassword(newEmail, newPassword)
+        .signInWithEmailAndPassword(userEmail, userPassword)
         .then(data => {
           console.log(data);
         })
@@ -68,7 +64,7 @@ $(document).ready(function() {
         });
     });
   
-    $("#submitButton").on("click", function(event) {
+    $("#createButton").on("click", function(event) {
       var _this = this;
       event.preventDefault();
       
@@ -87,7 +83,6 @@ $(document).ready(function() {
       var newUser = {
         email: emailAddress,
         password: firstPassword
-        username: 
       };
       console.log(newUser);
   
@@ -122,7 +117,7 @@ $(document).ready(function() {
           user => {
             console.log('moving user');
             console.log(user);
-            window.location = "index.html";
+            window.location = "index3.html";
           },
           error => {
             // Handle Errors here.
@@ -133,55 +128,6 @@ $(document).ready(function() {
           }
         );
       }
-    });
-  
-    //when the user clicks the login with facebook button, the popup appears to sign in with facebook.
-    $("#facebook").click(function() {
-      var provider = new firebase.auth.FacebookAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // ...
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
-    });
-  
-    $("#twitter").click(function() {
-      var provider = new firebase.auth.FacebookAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // ...
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
     });
   
     $("#forgot").on("click", evt => {
